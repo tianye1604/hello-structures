0fc1e26d476c3b978bec74447f26ff8679a6d92c
Tian.shujian <tianye1604@163.com>
2019-06-26 12:47:21 +0800

    add any method

--- helloarray/src/main/java/com/hello/structrue/array/Array.java
+++ helloarray/src/main/java/com/hello/structrue/array/Array.java
@@ -49,4 +49,56 @@ public class Array {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    /**
+     *  获取数组中的元素
+     * @param index
+     * @return
+     */
+    public int get(int index) {
+        if(size <= index){
+            throw new IllegalArgumentException("Get is fail,index is illegal!");
+        }
+        return data[index];
+    }
+
+    /**
+     * 设置数组指定位置中的元素
+     * @param index
+     * @param e
+     */
+    public void set(int index,int e){
+        if(index<0 || size <= index){
+            throw new IllegalArgumentException("Set is fail,index is illegal!");
+        }
+        data[index] = e;
+    }
+
+    /**
+     * 在数组的指定位置,添加元素
+     * @param index
+     * @param e
+     */
+    public void add(int index,int e) {
+        if(index<0 || index > size){
+            throw new IllegalArgumentException("add is fail,index is illegal!");
+        }
+        for (int i=size-1; i>=index;i--) {
+            data[i] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    /**
+     * 在数组的头部,添加元素
+     * @param e
+     */
+    public void addFirst(int e){
+        add(0,e);
+    }
+
+    public void addLast(int e){
+        add(size,0);
+    }
 }
